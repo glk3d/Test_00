df4d49d6112bf0699b5190a836ddc020866e56bd
glk3d <gl@karamba3d.com>
2024-07-31 13:34:30 +0200

    Test

--- SpeckleAutomateDotnetExample/AutomateFunction.cs
+++ SpeckleAutomateDotnetExample/AutomateFunction.cs
@@ -4,35 +4,41 @@ using Speckle.Core.Models.Extensions;
 
 public static class AutomateFunction
 {
-  public static async Task Run(
-    AutomationContext automationContext,
-    FunctionInputs functionInputs
-  )
-  {
-    Console.WriteLine("Starting execution");
-    _ = typeof(ObjectsKit).Assembly; // INFO: Force objects kit to initialize
-
-    Console.WriteLine("Receiving version");
-    var commitObject = await automationContext.ReceiveVersion();
-
-    Console.WriteLine("Received version: " + commitObject);
-
-    Console.WriteLine(commitObject.TryGetName());
-
-    var count = commitObject
-      .Flatten()
-      .Count(b => b.speckle_type == functionInputs.SpeckleTypeToCount);
-
-    Console.WriteLine($"Counted {count} objects");
-
-    if (count < functionInputs.SpeckleTypeTargetCount)
+    public static async Task Run(AutomationContext automationContext, FunctionInputs functionInputs)
     {
-      automationContext.MarkRunFailed(
-        $"Counted {count} objects where {functionInputs.SpeckleTypeTargetCount} were expected"
-      );
-      return;
-    }
+        _ = typeof(ObjectsKit).Assembly; // INFO: Force objects kit to initialize
 
-    automationContext.MarkRunSuccess($"Counted {count} objects");
-  }
+        Console.WriteLine("Starting execution");
+
+        Console.WriteLine("Receiving version");
+        var commitObject = await automationContext.ReceiveVersion();
+        Console.WriteLine("Received version: " + commitObject);
+
+        await automationContext.CreateNewVersionInProject(commitObject, "Another Model", "Fünf");
+
+        if (commitObject.applicationId.Contains("Karamba3D"))
+        {
+            automationContext.MarkRunSuccess($"Commit is from Karamba3D Model.");
+        }
+
+        Console.WriteLine(commitObject.TryGetName());
+
+        var count = commitObject
+          .Flatten()
+          .Count(b => b.speckle_type == functionInputs.SpeckleTypeToCount);
+
+        Console.WriteLine($"Counted {count} objects");
+
+        /*
+        if (count < functionInputs.SpeckleTypeTargetCount)
+        {
+          automationContext.MarkRunFailed(
+            $"Counted {count} objects where {functionInputs.SpeckleTypeTargetCount} were expected"
+          );
+          return;
+        }
+        */
+
+        automationContext.MarkRunSuccess($"Counted {count} objects");
+    }
 }
--- TestAutomateFunction/AutomationContextTest.cs
+++ TestAutomateFunction/AutomationContextTest.cs
@@ -1,53 +1,74 @@
 namespace TestAutomateFunction;
 
 using Speckle.Automate.Sdk;
+using Speckle.Automate.Sdk.Schema;
 using Speckle.Automate.Sdk.Test;
 using Speckle.Core.Api;
 using Speckle.Core.Credentials;
 
+
+/*
+  cd38d517485dfce646e20af2ac69486284b1eb7cd6
+  af744e71e84e9a9c69d8f6edc57ee15288933ef965
+*/
+
 [TestFixture]
 public sealed class AutomationContextTest : IDisposable
 {
-  private Client client;
-  private Account account;
+    private Client client;
+    private Account account;
 
-  [OneTimeSetUp]
-  public void Setup()
-  {
-    account = new Account
+    [OneTimeSetUp]
+    public void Setup()
     {
-      token = TestAutomateEnvironment.GetSpeckleToken(),
-      serverInfo = new ServerInfo
-      {
-        url = TestAutomateEnvironment.GetSpeckleServerUrl().ToString()
-      }
-    };
-    client = new Client(account);
-  }
+        var token = TestAutomateEnvironment.GetSpeckleToken();
 
-  [Test]
-  public async Task TestFunctionRun()
-  {
-    var inputs = new FunctionInputs
+        var url = TestAutomateEnvironment.GetSpeckleServerUrl().ToString();
+        var serverInfo = new ServerInfo { url = url };
+
+        account = new Account
+        {
+            token = token,
+            serverInfo = serverInfo
+        };
+
+        client = new Client(account);
+    }
+
+    [Test]
+    public async Task TestFunctionRun()
     {
-      SpeckleTypeToCount = "Base",
-      SpeckleTypeTargetCount = 1
-    };
+        var inputs = new FunctionInputs
+        {
+            SpeckleTypeToCount = "Base",
+            SpeckleTypeTargetCount = 1,
+            ExternalServiceKey = "FÜNF",
+        };
 
-    var automationRunData = await TestAutomateUtils.CreateTestRun(client);
-    var automationContext = await AutomationRunner.RunFunction(
-      AutomateFunction.Run,
-      automationRunData,
-      account.token,
-      inputs
-    );
+        /*
+        var automationRunData = new AutomationRunData()
+        {
+            ProjectId = TestAutomateEnvironment.GetSpeckleProjectId(),
+            AutomationId = TestAutomateEnvironment.GetSpeckleAutomationId(),
+            SpeckleServerUrl = TestAutomateEnvironment.GetSpeckleServerUrl().ToString(),
+        };
+        */
+        var automationRunData = await TestAutomateUtils.CreateTestRun(client);
 
-    Assert.That(automationContext.RunStatus, Is.EqualTo("SUCCEEDED"));
-  }
 
-  public void Dispose()
-  {
-    client.Dispose();
-    TestAutomateEnvironment.Clear();
-  }
+        var automationContext = await AutomationRunner.RunFunction(
+          AutomateFunction.Run,
+         automationRunData,
+          account.token,
+          inputs
+        );
+
+        Assert.That(automationContext.RunStatus, Is.EqualTo("SUCCEEDED"));
+    }
+
+    public void Dispose()
+    {
+        client.Dispose();
+        TestAutomateEnvironment.Clear();
+    }
 }
